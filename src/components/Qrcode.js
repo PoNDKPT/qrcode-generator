824e98c509fb2d650372bc97c1d00374638653e5
PoNDKPT <sakon.kpt@gmail.com>
2022-06-06 16:12:33 +0700

    Add Static QRCode

--- src/components/Qrcode.js
+++ src/components/Qrcode.js
@@ -4,6 +4,7 @@ import QRCode from 'qrcode';
 function Qrcode() {
   const [url, setUrl] = useState('');
   const [qr, setQr] = useState('');
+  const staticQrcode = '/image/static-qrcode.png';
 
   const GenerateQRCode = () => {
     QRCode.toDataURL(
@@ -19,6 +20,7 @@ function Qrcode() {
       }
     );
   };
+
   return (
     <div className="h-screen w-4/5 mx-auto flex flex-col justify-center lg:flex-row lg:items-center">
       <div className="lg:w-1/2">
@@ -46,19 +48,17 @@ function Qrcode() {
       </div>
       <div className="lg:w-1/2 flex flex-col items-center lg:items-end">
         <div className="w-64 h-64 mt-10 border border-gray-400 md:mt-24 lg:w-96 lg:h-96 lg:mt-0">
-          <img src={qr} />
+          <img src={qr ? qr : staticQrcode} />
         </div>
-        {qr && (
-          <div className="rounded-md flex justify-center mt-6 w-64 lg:w-96">
-            <a
-              className="w-max flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-8 md:w-max"
-              href={qr}
-              download="qrcode.png"
-            >
-              Download
-            </a>
-          </div>
-        )}
+        <div className="rounded-md flex justify-center mt-6 w-64 lg:w-96">
+          <a
+            className="w-max flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-8 md:w-max"
+            href={qr ? qr : staticQrcode}
+            download="qrcode.png"
+          >
+            Download
+          </a>
+        </div>
       </div>
     </div>
   );
